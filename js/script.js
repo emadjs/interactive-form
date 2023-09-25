a2d2f6d4026e542c220d1a73c0647e45fe4c0a51
Sub Zero <121079771+ersync@users.noreply.github.com>
2023-09-26 00:57:26 +0330

    add functionality to 'Register for Activities' section

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
+// Basic Info
 document.querySelector("#name").focus()
-
 const otherJobInput = document.querySelector("#other-job-role")
 otherJobInput.style.display = "none"
 
@@ -10,7 +10,7 @@ jobRoleSelect.addEventListener("change", (e) => {
     } else
         otherJobInput.style.display = "none"
 })
-
+// T-Shirt Info
 const shirtColorsSelect = document.querySelector("#color")
 shirtColorsSelect.disabled = true
 
@@ -20,7 +20,69 @@ shirtDesignSelect.addEventListener("change", (e) => {
     console.log();
     [].forEach.call(shirtColorsSelect.options, (option) => {
         option.getAttribute("data-theme") === e.target.value ? option.style.display = "" : option.style.display = "none"
-
     })
-
 })
+
+// Register for Activities *
+let totalCost = 0
+const activities = document.querySelector("#activities")
+let activitiesCost = document.querySelector("#activities-cost")
+
+activities.addEventListener("change", (e) => {
+    const currentActivity = e.target
+    const conflictActivities = findConflicts(currentActivity)
+    disableConflicts(currentActivity, conflictActivities)
+    const cost = +e.target.parentNode.querySelector(".activity-cost").textContent.slice(1)
+    e.target.checked ? totalCost += cost : totalCost -= cost
+    activitiesCost.textContent = `Total: $${totalCost}`
+})
+
+function readTime(str) {
+    if (!str) return
+    let [start, end] = str.split("-")
+    startNumber = parseInt(start)
+    startNumber = startNumber + (start.includes("pm") && startNumber < 12 ? 12 : 0)
+    endNumber = parseInt(end)
+    endNumber = endNumber + (end.includes("pm") && endNumber < 12 ? 12 : 0)
+    return {startNumber, endNumber}
+}
+
+
+function findConflicts(activity) {
+    const activityObjects = []
+    const inputs = activities.querySelectorAll("input")
+    Array.from(inputs).forEach((input) => {
+        const fullTime = input.dataset.dayAndTime?.split(" ")[1]
+        activityObjects.push({
+            input,
+            activityName: input.name,
+            day: input.dataset.dayAndTime?.split(" ")[0],
+            startTime: readTime(fullTime)?.startNumber,
+            endTime: readTime(fullTime)?.endNumber,
+        })
+    })
+    const activityObject = activityObjects.find(act => act.activityName === activity.getAttribute("name"))
+    return activityObjects.filter(act =>
+        act.activityName !== activityObject.activityName &&
+        act.day === activityObject.day &&
+        act.startTime <= activityObject.endTime &&
+        act.endTime >= activityObject.endTime
+    )
+}
+
+function disableConflicts(currentActivity, arrOfActivities) {
+    arrOfActivities.forEach(activity => {
+        currentActivity.checked ? activity.input.disabled = true : activity.input.disabled = false
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
